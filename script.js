61bd353ee42dd9583d73388e80b364c5f1e40bfc
Dylan Nelson <nlsdyl001@myuct.ac.za>
2022-10-08 19:57:12 +0200

    use mathjax to render solutions

--- script.js
+++ script.js
@@ -14,12 +14,18 @@ function findSolutions() {
     if (worker !== null) {
         worker.terminate()
     }
+
+    for (const label of resultLabels) {
+        label.innerHTML = ""
+    }
+
     worker = new Worker("./solver_worker.js")
     worker.onmessage = function(event) {
         const solution = event.data
         console.log(solution.value.numerator + " = " + solution.expression)
         if (solution.value.numerator <= 100n) {
-            resultLabels[Number(solution.value.numerator)].innerHTML = solution.expression
+            resultLabels[Number(solution.value.numerator) - 1].innerHTML = "$$" + solution.value.numerator + " = " + solution.expression + "$$"
+            MathJax.typeset()
         }
     }
     worker.postMessage({ year: yearInput.value })
