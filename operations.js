f65f48bd542b961873c32ee0282e4980458fa9f7
Dylan Nelson <nlsdyl001@myuct.ac.za>
2022-10-08 12:17:49 +0200

    add method to combine to operations using subtraction

--- operations.js
+++ operations.js
@@ -26,16 +26,33 @@ class Operation {
 
     static add(left, right) {
         // Prefer (a + b) + c to a + (b + c)
-        if (right.operationType == OperationTypes.Add) return undefined
+        if (right.operationType === OperationTypes.Add) return undefined
         // Prefer (a + b) - c to a + (b - c)
-        if (right.operationType == OperationTypes.Subtract) return undefined
+        if (right.operationType === OperationTypes.Subtract) return undefined
         // Prefer a - b to a + (-b)
-        if (right.operationType == OperationTypes.UnaryMinus) return undefined
+        if (right.operationType === OperationTypes.UnaryMinus) return undefined
         return new Operation(
             OperationTypes.Add,
             left.numberOfOperations + right.numberOfOperations + 1,
             left.expression + ' + ' + right.expression,
             Fraction.add(left.value, right.value) 
+        )
+    }
+
+    static sub(left, right) {
+        // Prefer (a - b) - c to a - (b + c)
+        if (right.operationType === OperationTypes.Add) return undefined
+        // Prefer (a - b) + c to a - (b - c)
+        if (right.operationType === OperationTypes.Subtract) return undefined
+        // Prefer a + b to a - (-b)
+        if (right.operationType === OperationTypes.UnaryMinus) return undefined
+        // Prefer a + 0 to a - 0
+        if (right.value.numerator === 0n) return undefined
+        return new Operation(
+            OperationTypes.Subtract,
+            left.numberOfOperations + right.numberOfOperations + 1,
+            left.expression + ' - ' + right.expression,
+            Fraction.sub(left.value, right.value)
         )
     }
 }
