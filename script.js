3d097d2eba45337ea622b8aac263b9074ff27c4b
Dylan Nelson <nlsdyl001@myuct.ac.za>
2022-10-09 04:23:34 +0200

    allow showing results for numbers larger than 100 as long as they appear in a consecutive block

--- script.js
+++ script.js
@@ -3,11 +3,18 @@ const resultsContainer = document.getElementById('results-container')
 const workingStatus = document.getElementById('working-status')
 const resultLabels = []
 
-for (let i = 0; i < 100; ++i) {
+let solutions = {}
+let numLabels = 100
+
+function createLabel() {
     const label = document.createElement('div')
     label.classList.add('solution')
     resultsContainer.append(label)
     resultLabels.push(label)
+}
+
+for (let i = 0; i < 100; ++i) {
+    createLabel()
 }
 resetLabels()
 
@@ -26,11 +33,14 @@ function typeset(num, expression, found = true) {
 function resetLabels() {
     workingStatus.style.display = 'none'
     MathJax.startup.output.clearCache()
+    resultLabels.splice(100)
+    numLabels = 100
     for (let i = 0; i < 100; i++) {
         const label = resultLabels[i];
         label.classList.remove('found')
         typeset(i + 1, '{\\color{gray}\\textit{No solution found}}', false)
     }
+    resultsContainer.replaceChildren(...resultLabels)
 }
 
 var worker = null
@@ -41,6 +51,7 @@ function findSolutions() {
     }
 
     resetLabels()
+    solutions = {}
 
     worker = new Worker('./solver_worker.js')
     worker.onmessage = function(event) {
@@ -48,10 +59,23 @@ function findSolutions() {
             workingStatus.style.display = 'none'
             return;
         }
+
         const solution = event.data
-        if (solution.value.numerator <= 100n) {
-            const num = Number(solution.value.numerator)
-            typeset(num, solution.expression)
+        const value = Number(solution.value.numerator)
+        if (value === Infinity) return
+        solutions[value] = solution
+
+        if (value <= numLabels) {
+            typeset(value, solution.expression)
+        }
+        else if (value === (numLabels + 1)) {
+            numLabels++
+            while (numLabels in solutions) {
+                createLabel()
+                typeset(numLabels, solutions[numLabels].expression)
+                numLabels++;
+            }
+            numLabels--
         }
     }
     worker.postMessage({ year: yearInput.value })
