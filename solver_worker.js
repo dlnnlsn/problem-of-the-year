4d7ddbc392cca120ab188a19f8d6aa3f8161efa8
Dylan Nelson <nlsdyl001@myuct.ac.za>
2022-10-11 10:38:29 +0200

    memoize solutions for segments

--- solver_worker.js
+++ solver_worker.js
@@ -8,7 +8,8 @@ onmessage = function (event) {
         for (const number of startingSet) {
             pruningEngine.registerNumber(number)
         }
-        for (const solution of findSolutions(startingSet, 0, startingSet.length)) {
+        const solver = new Solver(startingSet)
+        for (const solution of solver.solve(0, startingSet.length)) {
             if (solution.value.denominator !== 1n) continue
             const num = solution.value.numerator
             if (num <= 0n) continue
@@ -21,35 +22,92 @@ onmessage = function (event) {
 const unaryOperations = [Operation.factorial, Operation.unaryMinus, Operation.squareRoot]
 const binaryOperations = [Operation.add, Operation.sub, Operation.mul, Operation.div, Operation.exponentiate]
 
-/**
-* @param {Operation} number
-* @returns {Iterable.<Operation>}
-*/
-function* applyUnaryOperations(number) {
-    yield number
-    for (const op of unaryOperations) {
-        const res = pruningEngine.applyOperation(op, number)
-        if (res === undefined) continue
-        yield* applyUnaryOperations(res)
+class Solver {
+    numbers
+    solutionCache
+
+    constructor(numbers) {
+        this.numbers = numbers
+        this.solutionCache = {}
     }
-}
 
-/**
-* @param {Array<Operation>} numbers
-* @param {number} startIndex
-* @param {endIndex} endIndex
-* @returns {Iterable.<Operation>}
-*/
-function* findSolutions(numbers, startIndex, endIndex) {
-    if ((endIndex - startIndex) === 1) yield* applyUnaryOperations(numbers[startIndex])
+    /**
+    * @param {number} startIndex
+    * @param {number} endIndex
+    * @returns {boolean}
+    */
+    sectionInCache(startIndex, endIndex) {
+        if (!(startIndex in this.solutionCache)) return false
+        return endIndex in this.solutionCache[startIndex]
+    }
 
-    for (let i = startIndex + 1; i < endIndex; i++) {
-        for (const left of findSolutions(numbers, startIndex, i)) {
-            for (const right of findSolutions(numbers, i, endIndex)) {
-                for (const op of binaryOperations) {
-                    const res = pruningEngine.applyOperation(op, left, right)
-                    if (res === undefined) continue
-                    yield* applyUnaryOperations(res)
+    /**
+    * @param {number} startIndex
+    * @param {number} endIndex
+    * @returns {Iterable.<Operation>}
+    */
+    yieldFromCache = function* (startIndex, endIndex) {
+        for (const numerator of Object.keys(this.solutionCache[startIndex][endIndex])) {
+            for (const denominator of Object.keys(
+                this.solutionCache[startIndex][endIndex][numerator]
+            )) {
+                yield this.solutionCache[startIndex][endIndex][numerator][denominator]
+            }
+        }
+    }
+
+    /**
+    * @param {number} startIndex
+    * @param {number} endIndex
+    * @param {Operation} number
+    * @returns {Iterable.<Operation>}
+    */
+    yieldNumber = function* (startIndex, endIndex, number) {
+        this.solutionCache[startIndex] ||= {}
+        this.solutionCache[startIndex][endIndex] ||= {}
+        const {numerator, denominator} = number.value
+        this.solutionCache[startIndex][endIndex][numerator] ||= {}
+        this.solutionCache[startIndex][endIndex][numerator][denominator] = number
+        yield number
+    }
+
+    /**
+    * @param {number} startIndex
+    * @param {number} endIndex
+    * @param {Operation} number
+    * @returns {Iterable.<Operation>}
+    */
+    applyUnaryOperations = function* (startIndex, endIndex, number) {
+        yield* this.yieldNumber(startIndex, endIndex, number)
+        for (const op of unaryOperations) {
+            const res = pruningEngine.applyOperation(op, number)
+            if (res === undefined) continue
+            yield* this.applyUnaryOperations(startIndex, endIndex, res)
+        }
+    }
+
+    /**
+    * @param {number} startIndex
+    * @param {number} endIndex
+    * @returns {Iterable.<Operation>}
+    */
+    solve = function* (startIndex, endIndex) {
+        if (this.sectionInCache(startIndex, endIndex)) {
+            yield* this.yieldFromCache(startIndex, endIndex)
+            return
+        }
+
+        if ((endIndex - startIndex) === 1)
+            yield* this.applyUnaryOperations(startIndex, endIndex, this.numbers[startIndex])
+
+        for (let i = startIndex + 1; i < endIndex; i++) {
+            for (const left of this.solve(startIndex, i)) {
+                for (const right of this.solve(i, endIndex)) {
+                    for (const op of binaryOperations) {
+                        const res = pruningEngine.applyOperation(op, left, right)
+                        if (res === undefined) continue
+                        yield* this.applyUnaryOperations(startIndex, endIndex, res)
+                    }
                 }
             }
         }
